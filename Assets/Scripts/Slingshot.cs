b6143d9f6d0f96ddb28eb270739f5072bf55c7eb
at821 <36107231+at821@users.noreply.github.com>
2018-09-27 21:48:09 -0400

    587

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+    //fields set in the Unity Inspector pane
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    //filds set dynamically
+    [Header("Set Dynamically")]
+
+
+    public GameObject launchPoint;
+
+    void Awake()    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+	void OnMouseEnter () {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit ()    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+}
